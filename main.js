a56619d4e20466054793fc6ca34839a0a25d6bf3
Thiago Nascimento <126019481+thiagogja27@users.noreply.github.com>
2024-01-29 14:50:05 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const nameElement = document.querySelector('#name');
+    const usernameElement = document.querySelector('#username');
+    const avatarElement = document.querySelector('#avatar');
+    const reposElement = document.querySelector('#repos');
+    const followersElement = document.querySelector('#followers');
+    const followingElement = document.querySelector('#following');
+    const linkElement = document.querySelector('#link');
+
+    fetch('https://api.github.com/users/thiagogja27')
+        .then(function (res) {
+            return res.json(); 
+        })
+        .then(function (json) {
+            nameElement.innerText = json.name;
+            usernameElement.innerText = json.login;
+            avatarElement.src = json.avatar_url; 
+            followersElement.innerText = json.followers;
+            followingElement.innerText = json.following;
+            reposElement.innerText = json.public_repos; 
+            linkElement.href = json.html_url;
+        })
+        .catch(function (error) {
+            console.log('Erro na solicitação:', error);
+        });
+});
